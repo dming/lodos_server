cbc19b663fcddf90bb6cc7df9ca1bcc81a4366b9
dming <501640144@qq.com>
2017-06-16 01:51:46 +0800

    server 0616

---
+++ gate/module.go
@@ -0,0 +1,70 @@
+/**
+一定要记得在confin.json配置这个模块的参数,否则无法使用
+*/
+package gate
+
+import (
+	"fmt"
+	"github.com/dming/lodos/conf"
+	"github.com/dming/lodos/gate"
+	log "github.com/dming/lodos/mlog"
+	"github.com/dming/lodos/module"
+
+)
+
+var Module = func() module.Module {
+	gate := new(Gate)
+	return gate
+}
+
+type Gate struct {
+	gate.Gate //继承
+}
+
+func (gate *Gate) GetType() string {
+	//很关键,需要与配置文件中的Module配置对应
+	return "Gate"
+}
+func (gate *Gate) Version() string {
+	//可以在监控时了解代码版本
+	return "1.0.0"
+}
+func (gate *Gate) OnInit(app module.AppInterface, settings conf.ModuleSettings) {
+	//注意这里一定要用 gate.Gate 而不是 module.BaseModule
+	gate.Gate.OnInit(app, settings)
+	gate.Gate.SetStorageHandler(gate) //设置持久化处理器
+}
+
+/**
+存储用户的Session信息
+Session Bind Userid以后每次设置 settings都会调用一次Storage
+*/
+func (gate *Gate) Storage(Userid string, settings map[string]string) (err error) {
+	log.Info("需要处理对Session的持久化")
+	return nil
+}
+
+/**
+强制删除Session信息
+*/
+func (gate *Gate) Delete(Userid string) (err error) {
+	log.Info("需要删除Session持久化数据")
+	return nil
+}
+
+/**
+获取用户Session信息
+用户登录以后会调用Query获取最新信息
+*/
+func (gate *Gate) Query(Userid string) (settings map[string]string, err error) {
+	log.Info("查询Session持久化数据")
+	return nil, fmt.Errorf("no redis")
+}
+
+/**
+用户心跳,一般用户在线时60s发送一次
+可以用来延长Session信息过期时间
+*/
+func (gate *Gate) Heartbeat(Userid string) {
+	log.Info("用户在线的心跳包")
+}
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/dming/lodos"
+	"server/gate"
+)
+//func ChatRoute( app module.App,Type string,hash string) (*module.ServerSession){
+//	//演示多个服务路由 默认使用第一个Server
+//	log.Debug("Hash:%s 将要调用 type : %s",hash,Type)
+//	servers:=app.GetServersByType(Type)
+//	if len(servers)==0{
+//		return nil
+//	}
+//	return servers[0]
+//}
+func main() {
+	app := lodos.CreateApp()
+	//app.Route("Chat",ChatRoute)
+	app.Run(true, //只有是在调试模式下才会在控制台打印日志, 非调试模式下只在日志文件中输出日志
+		gate.Module(),  //这是默认网关模块,是必须的支持 TCP,websocket,MQTT协议
+		//login.Module(), //这是用户登录验证模块
+		//chat.Module(),
+		//user.Module(),
+		//webapp.Module(),
+	)
+
+}
+
